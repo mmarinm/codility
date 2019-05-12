4f8fbec95d9966ce50858059e3e295df06636260
mmarinm <mestrovic.ma@gmail.com>
2019-05-12 18:59:35 -0400

    [WiP]

---
+++ frogJump.js
@@ -0,0 +1,8 @@
+// sample test values
+// (10, 85, 30)  => 3
+// (5, 105, 3)   => 34
+
+function solution(X, Y, D) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    return Math.ceil((Y-X) / D)
+}
