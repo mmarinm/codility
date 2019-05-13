ce631fce33f4917566e410630e8712d68dbe3f4a
mmarinm <mestrovic.ma@gmail.com>
2019-05-13 06:33:17 -0400

    ""

--- permCheck.js
+++ permCheck.js
@@ -2,13 +2,11 @@ function solution(A) {
     const counter = new Array(A.length).fill(0);
     for (let index = 0; index < A.length; index++) {
         const element = A[index];
-        if (1 < element > A.length) return 0
+        if (element < 1 || element > A.length) return 0
         if(counter[element-1] !== 0) return 0
         counter[element - 1] = 1
     }
     return 1
-    
-
 }
 
 
@@ -16,6 +14,7 @@ const A = [4,1,3,2]  //1
 const B = [4,1,3]  //0
 const C = [1, 1]   //0
 const D = [1]   //1
+const E = [-4,1,-3,2]  //1
 
 console.log('A', A);
 console.log(solution(A));
@@ -30,6 +29,9 @@ console.log(solution(C));
 
 console.log('D', D);
 console.log(solution(D));
+
+console.log('E', E);
+console.log(solution(E));
 
 // solution(A)
 // solution(B)
